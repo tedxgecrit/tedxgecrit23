1ed7f562cd694656497cbfaad1d5d4d07476c928
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2023-08-15 19:07:04 +0530

    Color updated

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -8,7 +8,7 @@ export default function Highlights() {
     <div className='h-max mb-[550px]'>
         <Heading >Highlights</Heading>
         <div className="h-auto w-full flex flex-col content-center items-center md:mt-[70px] mt-[15px]">
-            <div className="md:w-[75%] w-[95%] md:h-[266px] h-[140px] bg-red absolute md:mt-[220px] mt-[110px]"></div>
+            <div className="md:w-[75%] w-[95%] md:h-[266px] h-[140px] bg-[#DA0400] absolute md:mt-[220px] mt-[110px]"></div>
             <div className="md:w-[65%] w-[90%] md:h-[439px] h-[230px] absolute"><VideoPlayer /></div>
         </div>
     </div>
