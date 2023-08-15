d3c01d5850a5514b6f157028eb1dec410a52dbca
GitHub Action <action@github.com>
2023-08-15 23:18:44 +0000

    style: format code with prettier

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,18 +1,19 @@
 import Container from "@/components/utility/Container";
 import Heading from "@/components/utility/Heading";
-import VideoPlayer from '@/components/Highlights/VideoPlayer'
+import VideoPlayer from "@/components/Highlights/VideoPlayer";
 
 export default function Highlights() {
-   return (
-   <Container section='highlights'>
-    <div className='h-max mb-[550px]'>
+  return (
+    <Container section="highlights">
+      <div className="mb-[550px] h-max">
         <Heading>Highlights</Heading>
-        <div className="h-auto w-full flex flex-col content-center items-center md:mt-[70px] mt-[15px]">
-            <div className="lg:rounded-[5px] md:w-[75%] w-[95%] md:h-[286px] h-[130px] bg-[#DA0400] absolute md:mt-[220px] mt-[110px"></div>
-            <div className="md:w-[65%] w-[90%] md:h-[439px] h-[230px] absolute"><VideoPlayer /></div>
+        <div className="mt-[15px] flex h-auto w-full flex-col content-center items-center md:mt-[70px]">
+          <div className="mt-[110px absolute h-[130px] w-[95%] bg-[#DA0400] md:mt-[220px] md:h-[286px] md:w-[75%] lg:rounded-[5px]"></div>
+          <div className="absolute h-[230px] w-[90%] md:h-[439px] md:w-[65%]">
+            <VideoPlayer />
+          </div>
         </div>
-    </div>
+      </div>
     </Container>
-    );
-    
+  );
 }
--- src/components/Highlights/VideoPlayer.js
+++ src/components/Highlights/VideoPlayer.js
@@ -4,7 +4,7 @@ const VideoPlayer = () => {
     <iframe
       width="100%"
       height="100%"
-    //   className="rounded-tl-[40px] rounded-br-[40px]"
+      //   className="rounded-tl-[40px] rounded-br-[40px]"
       src="https://www.youtube.com/embed/Cb91MpODfBU"
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
