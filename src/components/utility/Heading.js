2370531324e235e1eeb6206238718d7ca1ad342d
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2023-08-15 19:20:05 +0530

    Heading margin added in tablet view

--- src/components/utility/Heading.js
+++ src/components/utility/Heading.js
@@ -1,6 +1,6 @@
 const Heading = (props) => {
   return (
-    <h1 className="text-5xl font-[200] uppercase max-sm:p-5 md:pb-10 lg:pb-5 lg:text-[60px]">
+    <h1 className="sm:ml-[25px] md:ml-[50px] lg:ml-0 text-5xl font-[200] uppercase max-sm:p-5 md:pb-10 lg:pb-5 lg:text-[60px]">
       {props?.children}
     </h1>
   );
