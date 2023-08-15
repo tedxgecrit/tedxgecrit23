006cb4a57beff0fec1779e1b887bb0ae087aca62
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2023-08-15 19:04:49 +0530

    Refactored code and resolved console errors.

--- src/components/Highlights/Videoplayer.js
+++ src/components/Highlights/Videoplayer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Videoplayer = () => {
+const VideoPlayer = () => {
   const src = "https://www.youtube.com/watch?v=l_9lOzqoXsA";
   return (
     <iframe
@@ -9,11 +9,10 @@ const Videoplayer = () => {
     //   className="rounded-tl-[40px] rounded-br-[40px]"
       src="https://www.youtube.com/embed/Cb91MpODfBU"
       title="YouTube video player"
-      frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
+      allowFullScreen
     ></iframe>
   );
 };
 
-export default Videoplayer;
+export default VideoPlayer;
