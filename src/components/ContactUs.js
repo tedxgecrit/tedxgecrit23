c2a4ae6667415dc9b1d2e260a9ebd45cb8403629
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2023-08-16 22:43:26 +0530

    Changes

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,5 +1,5 @@
-import Container from "./utility/Container";
-import Heading from "./utility/Heading";
+import Container from "@/components/utility/Container";
+import Heading from "@/components/utility/Heading";
 
 export default function ContactUs() {
     return(
@@ -7,16 +7,15 @@ export default function ContactUs() {
             <div className="w-full h-screen">
                 <Heading>Contact</Heading>
                 <div className="flex flex-col items-center text-center">
-                <div className="md:w-[40%] w-[73%] pt-[25px] text-center text-[24px] font-light leading-relaxed">Feel free to contact us if you have any questions, comments or concerns</div>
+                <div className="md:w-[40%] w-[73%] pt-[25px] text-center md:text-[24px] text-[20px] font-light">Feel free to contact us if you have any questions, comments or concerns</div>
                 <div className="w-full h-[385px] mt-10">
-                    <div className="mb-[25px]"><input placeholder="Name" type="text" className="md:w-[50%] w-[82%] h-[45px]  rounded-t-[5px] bg-[#1B1B1B] border-b-2 border-[#DA0400] placeholder:pl-[10px] placeholder:text-[20px] placeholder:font-light"/></div>
-                    <div className="mb-[25px]"><input placeholder="Email/Phone" type="text" className="md:w-[50%] w-[82%] h-[45px]  rounded-t-[5px] bg-[#1B1B1B] border-b-2 border-[#DA0400] placeholder:pl-[10px]  placeholder:text-[20px] placeholder:font-light"/></div>
-                    <textarea placeholder="Your message..." type="text" className="md:w-[50%] w-[82%] h-[220px] rounded-t-[5px] bg-[#1B1B1B] border-b-2 border-[#DA0400] placeholder:pl-[10px] placeholder:pt-[10px]  placeholder:text-[20px] placeholder:font-light"/>
+                    <div className="mb-[25px]"><input placeholder="Name" type="text" className="focus:outline-none md:w-[50%] w-[82%] h-[45px]  rounded-t-[5px] bg-[#1B1B1B] border-b-2 border-[#DA0400] pl-[10px] placeholder:text-[18px] placeholder:font-light" required/></div>
+                    <div className="mb-[25px]"><input placeholder="Email/Phone" type="text" className="focus:outline-none md:w-[50%] w-[82%] h-[45px]  rounded-t-[5px] bg-[#1B1B1B] border-b-2 border-[#DA0400] pl-[10px] placeholder:text-[18px] placeholder:font-light"required/></div>
+                    <textarea placeholder="Your message..." type="text" className="focus:outline-none resize-none md:w-[50%] w-[82%] h-[220px] rounded-t-[5px] bg-[#1B1B1B] border-b-2 border-[#DA0400] pl-[10px] pt-[10px]  placeholder:text-[18px] placeholder:font-light" required/>
                 </div>
-                    <button className="md:w-[280px] w-[232px] h-[50px] mt-[30px] bg-[#DA0400] rounded-[5px] text-[20px] font-semibold">SEND MESSAGE</button>                
+                    <button className="md:w-[280px] w-[232px] h-[50px] md:mt-[30px] mt-[10px] bg-[#DA0400] rounded-[5px] text-[20px] font-semibold">SEND MESSAGE</button>                
                 </div>
             </div>
         </Container>
-    );
-    
+    );   
 }
