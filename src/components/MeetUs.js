6644c5037a02264281e56fcd35a33da37f9953b3
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2023-08-17 22:26:54 +0530

    Updated colors and leading

--- src/components/MeetUs.js
+++ src/components/MeetUs.js
@@ -3,12 +3,12 @@ import Container from "@/components/utility/Container";
 export default function MeetUs() {
   return (
     <Container section="meetus">
-      <div className="mb-[100px] flex h-[600px]   w-full flex-col items-center justify-center text-center">
-        <div className="mb-[20px] w-[89%] text-[22px] font-light md:w-[75%] md:text-[24px]">
+      <div className="w-full h-[600px] flex  flex-col items-center justify-center text-center mb-[100px]">
+        <div className="md:w-[75%] w-[89%] text-[22px] font-light md:text-[24px] mb-[20px] leading-tight">
           This initiative would not have been possible without the generous
           support of our sponsors and team members. ❤️
         </div>
-        <button className="h-[50px] w-[232px] rounded-[5px] bg-[#DA0400] text-[20px] font-semibold md:w-[280px]">
+        <button className="h-[50px] w-[232px] rounded-[5px] bg-red text-[20px] font-semibold md:w-[280px]">
           MEET THEM
         </button>
       </div>
