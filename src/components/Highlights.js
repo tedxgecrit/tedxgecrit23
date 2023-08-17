4c0c66e594e4d3578c9499e277c506e24596f7ce
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2023-08-17 22:16:03 +0530

    Formatted document

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -3,16 +3,17 @@ import Heading from "@/components/utility/Heading";
 import VideoPlayer from "@/components/Highlights/VideoPlayer";
 
 export default function Highlights() {
-   return (
-   <Container section='highlights'>
-    <div>
+  return (
+    <Container section="highlights">
+      <div>
         <Heading>Highlights</Heading>
-        <div className="md:h-[675px] h-[385px] w-full flex flex-col content-center items-center md:mt-[70px] mt-[15px]">
-            <div className="md:w-[75%] w-[95%] md:h-[266px] h-[140px] bg-[#DA0400] absolute md:mt-[220px] mt-[110px]"></div>
-            <div className="md:w-[65%] w-[90%] md:h-[439px] h-[230px] absolute"><VideoPlayer /></div>
+        <div className="mt-[15px] flex h-[385px] w-full flex-col content-center items-center md:mt-[70px] md:h-[675px]">
+          <div className="absolute mt-[110px] h-[140px] w-[95%] bg-[#DA0400] md:mt-[220px] md:h-[266px] md:w-[75%]"></div>
+          <div className="absolute h-[230px] w-[90%] md:h-[439px] md:w-[65%]">
+            <VideoPlayer />
+          </div>
         </div>
-    </div>
+      </div>
     </Container>
-    );
-    
+  );
 }
--- src/components/MeetUs.js
+++ src/components/MeetUs.js
@@ -1,12 +1,17 @@
 import Container from "@/components/utility/Container";
 
 export default function MeetUs() {
-    return(
-        <Container section='meetus'>
-            <div className="w-full h-[600px] mb-[100px]   flex flex-col items-center justify-center text-center">
-                <div className="md:w-[75%] w-[89%] md:text-[24px] text-[22px] mb-[20px] font-light">This initiative would not have been possible without the generous support of our sponsors and team members. ❤️</div>            
-                <button className="md:w-[280px] w-[232px] h-[50px] bg-[#DA0400] rounded-[5px] text-[20px] font-semibold">MEET THEM</button>            
-            </div>            
-        </Container>
-    );
+  return (
+    <Container section="meetus">
+      <div className="mb-[100px] flex h-[600px]   w-full flex-col items-center justify-center text-center">
+        <div className="mb-[20px] w-[89%] text-[22px] font-light md:w-[75%] md:text-[24px]">
+          This initiative would not have been possible without the generous
+          support of our sponsors and team members. ❤️
+        </div>
+        <button className="h-[50px] w-[232px] rounded-[5px] bg-[#DA0400] text-[20px] font-semibold md:w-[280px]">
+          MEET THEM
+        </button>
+      </div>
+    </Container>
+  );
 }
