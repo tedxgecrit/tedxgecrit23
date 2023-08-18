d8ed317f229e1e0a4283d0072f0dd608c04e2490
sreerag <msratofficial@gmail.com>
2023-08-18 16:12:27 +0530

    feat:add team component

---
+++ src/data/IconData.js
@@ -0,0 +1,9 @@
+import LinkedInIcon from "../../public/icons/linkedin.svg";
+import GithubIcon from "../../public/icons/github.svg";
+import InstagramIcon from "../../public/icons/instagram.svg";
+
+export const IconData = {
+  linkedin: { text: "linkedin", icon: LinkedInIcon },
+  github: { text: "github", icon: GithubIcon },
+  instagram: { text: "instagram", icon: InstagramIcon },
+};
--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -14,9 +14,11 @@ export const organizer = [
     "https://www.linkedin.com/in/gopikagopan1091/",
     null,
     null,
-    "/team/organizer/"
+    "/team/organizer/gopika.webp"
   ),
 ];
+
+
 
 const coreTeam = [
   new Volunteer(
@@ -24,44 +26,44 @@ const coreTeam = [
     null,
     null,
     "https://instagram.com/aneesara_murphy",
-    "/team/core/"
+    "/team/core/aneesa.webp"
   ),
-  new Volunteer("Sarath Suresh P", null, null, null, "/team/core/"),
+  new Volunteer("Sarath Suresh P", null, null, null, "/team/core/sarath.webp"),
   new Volunteer(
     "Elvis Jacob Ajay",
     null,
     null,
     "https://instagram.com/elvjac",
-    "/team/core/"
+    "/team/core/elvis.webp"
   ),
   new Volunteer(
     "Karthikeyan",
     null,
     null,
     "https://instagram.com/__being_.me",
-    "/team/core/"
+    "/team/core/karthikeyan.webp"
   ),
   new Volunteer(
     "Akash",
     null,
     null,
     "https://www.instagram.com/_akash.ps/",
-    "/team/core/"
+    "/team/core/akash.webp"
   ),
-  new Volunteer("Althaf Faisal", null, null, null, "/team/core/"),
+  new Volunteer("Althaf Faisal", null, null, null, "/team/core/althaf.webp"),
   new Volunteer(
     "A Divyesh",
     "https://www.linkedin.com/in/divyesh-ashok-205160251",
     null,
     null,
-    "/team/core/"
+    "/team/core/divyesh.webp"
   ),
   new Volunteer(
     "Aswin V M",
     "https://www.linkedin.com/in/aswin-v-m-899915256",
     null,
     null,
-    "/team/core/"
+    "/team/core/aswin.webp"
   ),
 ];
 
@@ -71,21 +73,28 @@ const contentTeam = [
     "https://www.linkedin.com/in/nandana-sathish-51014b230",
     null,
     null,
-    "/team/content/"
+    "/team/content/nandana.webp"
   ),
   new Volunteer(
     "Vignesh R Pillai",
     "https://www.linkedin.com/in/vigneshrpillai",
     null,
     null,
-    "/team/content/"
+    "/team/content/vignesh.webp"
   ),
   new Volunteer(
     "M S Navaneetha",
     null,
     null,
     "https://instagram.com/m_s_navaneetha",
-    "/team/content/"
+    "/team/content/navaneetha.webp"
+  ),
+  new Volunteer(
+    "Noel Joseph ",
+    null,
+    null,
+    "https://instagram.com/noel_joseph.az?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+    "/team/content/noel.webp"
   ),
 ];
 
@@ -95,35 +104,35 @@ const mediaTeam = [
     "https://www.linkedin.com/in/sterinroy/",
     null,
     null,
-    "/team/media/"
+    "/team/media/sterin.webp"
   ),
   new Volunteer(
     "Jinash Jaleel",
     "https://www.linkedin.com/in/jinash-jaleel",
     null,
     null,
-    "/team/media/"
+    "/team/media/jinash.webp"
   ),
   new Volunteer(
     "Jobin Jose",
     "https://www.linkedin.com/in/jobin-jose-73b60622a",
     null,
     null,
-    "/team/media/"
+    "/team/media/jobin.webp"
   ),
   new Volunteer(
     "Dilshith T S",
     null,
     null,
     "https://instagram.com/ab.normal.dil",
-    "/team/media/"
+    "/team/media/dilshith.webp"
   ),
   new Volunteer(
     "Rohit R",
     "https://www.linkedin.com/in/rohit-r-064692288",
     null,
     null,
-    "/team/media/"
+    "/team/media/rohith.webp"
   ),
 ];
 
@@ -133,28 +142,28 @@ const webTeam = [
     null,
     "https://github.com/alvin1904",
     null,
-    "/team/web/"
+    "/team/web/alvin.webp"
   ),
   new Volunteer(
     "Febin Nelson P",
     null,
     "https://github.com/fbn776",
     null,
-    "/team/web/"
+    "/team/web/febin.webp"
   ),
   new Volunteer(
     "Saurav K S",
     "https://www.linkedin.com/in/saurav-k-s-453894211/",
     null,
     null,
-    "/team/web/"
+    "/team/web/saurav.webp"
   ),
   new Volunteer(
     "Sreerag M",
     null,
     "https://github.com/sm0483",
     null,
-    "/team/web/"
+    "/team/web/sreerag.webp"
   ),
 ];
 
@@ -164,35 +173,35 @@ const technicalTeam = [
     null,
     null,
     "https://instagram.com/8th_trap",
-    "/team/technical/"
+    "/team/technical/sangeeth.webp"
   ),
   new Volunteer(
     "Anzif A",
     null,
     null,
     "https://instagram.com/anzeeeeeff",
-    "/team/technical/"
+    "/team/technical/anzif.webp"
   ),
   new Volunteer(
     "Mohammed Shakir PM",
     "https://www.linkedin.com/in/shakir-pm-7b942b288",
     null,
     null,
-    "/team/technical/"
+    "/team/technical/shakir.webp"
   ),
   new Volunteer(
     "Rahul S",
     null,
     null,
     "https://instagram.com/the__evil__eye__",
-    "/team/technical/"
+    "/team/technical/rahul_s.webp"
   ),
   new Volunteer(
     "Ahmed K M",
     null,
     null,
     "https://instagram.com/ahmxdkm",
-    "/team/technical/"
+    "/team/technical/ahmed.webp"
   ),
 ];
 
@@ -202,49 +211,49 @@ const marketingTeam = [
     "https://www.linkedin.com/in/thomas-kovoor-245811253",
     null,
     null,
-    "/team/marketing/"
+    "/team/marketing/thomas.webp"
   ),
   new Volunteer(
     "Afa Afsal KT",
     null,
     null,
     "https://instagram.com/__affaaa__",
-    "/team/marketing/"
+    "/team/marketing/afa.webp"
   ),
   new Volunteer(
     "Gangothri D Joy",
     "https://www.linkedin.com/in/gangothri-d-joy-22221721b",
     null,
     null,
-    "/team/marketing/"
+    "/team/marketing/gangothri.webp"
   ),
   new Volunteer(
     "Bharath Prasanth",
     "https://www.linkedin.com/in/bharath-prasanth-4a8ab9159",
     null,
     null,
-    "/team/marketing/"
+    "/team/marketing/bharath.webp"
   ),
   new Volunteer(
     "Anjaly Ajith",
     "https://www.linkedin.com/in/anjaly-ajith-630535254",
     null,
     null,
-    "/team/marketing/"
+    "/team/marketing/anjaly.webp"
   ),
   new Volunteer(
     "Roshan Thomas B V",
     "https://www.linkedin.com/in/roshan-thomas-b-v-4116b3273/",
     null,
     null,
-    "/team/marketing/"
+    "/team/marketing/roshan.webp"
   ),
   new Volunteer(
     "Aathira Jayadeep",
     "https://www.linkedin.com/in/aathira-jayadeep-781124276",
     null,
     null,
-    "/team/marketing/"
+    "/team/marketing/aathira.webp"
   ),
 ];
 
@@ -254,89 +263,96 @@ const sponsorshipTeam = [
     "https://www.linkedin.com/in/adithya-lakshmi-h-3485a4225",
     null,
     null,
-    "/team/sponsorship/"
+    "/team/sponsorship/adithya.webp"
   ),
-  new Volunteer("Bharath Krishnan", null, null, null, "/team/sponsorship/"),
+  new Volunteer(
+    "Bharath Krishnan",
+    null,
+    null,
+    null,
+    "/team/sponsorship/bharath_k.webp"
+  ),
   new Volunteer(
     "Shreya Balu",
     "https://www.linkedin.com/in/shreya-balu-6b0308248",
     null,
     null,
-    "/team/sponsorship/"
+    "/team/sponsorship/shreya.webp"
   ),
   new Volunteer(
     "Prabhav Narayanan",
     "https://www.linkedin.com/in/prabhav-narayanan-2919a6241",
     null,
     null,
-    "/team/sponsorship/"
+    "/team/sponsorship/prabhav.webp"
   ),
   new Volunteer(
     "Aiswarya M K",
     null,
     null,
     "https://instagram.com/aiswarya_krishnakumar_",
-    "/team/sponsorship/"
+    "/team/sponsorship/aishwarya.webp"
   ),
   new Volunteer(
     "Sanju Jose",
     "https://www.linkedin.com/in/sanju-jose-3ba8b31b6/",
     null,
     null,
-    "/team/sponsorship/"
+    "/team/sponsorship/sanju.webp"
   ),
   new Volunteer(
     "Karthika M Nambiar",
     "https://www.linkedin.com/in/karthika-nambiar-305ab2225/",
     null,
     null,
-    "/team/sponsorship/"
+    "/team/sponsorship/karthika.webp"
   ),
   new Volunteer(
     "Abhishekh Jayadev Nair",
     "https://www.linkedin.com/in/abhishekh-jayadev-nair-b36442225",
     null,
     null,
-    "/team/sponsorship/"
+    "/team/sponsorship/abhishekh.webp"
   ),
+  new Volunteer("Diya K A", null, null, null, "/team/sponsorship/diya.webp"),
 ];
 
 const eventTeam = [
-  new Volunteer("Julian Jose", null, null, null, "/team/event/"),
+  new Volunteer("Julian Jose", null, null, null, "/team/event/julian.webp"),
   new Volunteer(
     "Nadha K M",
     "https://www.linkedin.com/in/nadha-k-m-24b24b258",
     null,
     null,
-    "/team/event/"
+    "/team/event/nadha.webp"
   ),
   new Volunteer(
     "Liya Sosa Varkey",
     null,
     null,
     "https://instagram.com/l__i._ya",
-    "/team/event/"
+    "/team/event/liya.webp"
   ),
   new Volunteer(
     "Nithya Suresh",
     "https://www.linkedin.com/in/nithya-suresh-4b12b9276",
     null,
     null,
-    "/team/event/"
+    "/team/event/nithya.webp"
   ),
   new Volunteer(
     "Prathul Nath",
     "https://www.linkedin.com/in/prathul-nath-357272234",
     null,
     null,
-    "/team/event/"
+    "/team/event/prathul.webp"
   ),
   new Volunteer(
     "Jithin K",
     "https://www.linkedin.com/in/jithin-k-291975237",
     null,
     null,
-    "/team/event/"
+    "/team/event/jithin.webp"
   ),
 ];
 
@@ -346,70 +362,77 @@ const generalVolunteers = [
     "https://www.linkedin.com/in/easwari-nair-53a4b4230/",
     null,
     null,
-    "/team/general/"
+    "/team/general/easwari.webp"
   ),
   new Volunteer(
     "Edna",
     "https://www.linkedin.com/in/LillyEdnaJason",
     null,
     null,
-    "/team/general/"
+    "/team/general/edna.webp"
   ),
   new Volunteer(
     "Vyshnav N",
     "https://www.linkedin.com/in/vyshnav-n-318ab1283/",
     null,
     null,
-    "/team/general/"
+    "/team/general/vyshnav.webp"
   ),
   new Volunteer(
     "Amal S",
     null,
     null,
     "https://www.instagram.com/a___mal_s/",
-    "/team/general/"
+    "/team/general/amal_s.webp"
   ),
   new Volunteer(
     "Shanty Xavier",
     "https://www.linkedin.com/in/shanty-xavier-051b55266",
     null,
     null,
-    "/team/general/"
+    "/team/general/shanty.webp"
   ),
   new Volunteer(
     "Gowsal Nazer",
     null,
     null,
     "https://www.instagram.com/_gowsal_nazer_/",
-    "/team/general/"
+    "/team/general/gowsal.webp"
   ),
   new Volunteer(
     "Rahul K",
     null,
     null,
     "https://www.instagram.com/_rahu_l._/",
-    "/team/general/"
+    "/team/general/rahul.webp"
   ),
   new Volunteer(
     "Megha Joy",
     "https://www.linkedin.com/in/meghajoy",
     null,
     null,
-    "/team/general/"
+    "/team/general/megha.webp"
   ),
   new Volunteer(
     "Pooja B Nair",
     null,
     null,
     "https://www.instagram.com/_poooh__ja__/",
-    "/team/general/"
+    "/team/general/pooja.webp"
   ),
   new Volunteer(
     "Harikrishnan S",
     "https://www.linkedin.com/in/harikrishnan-s-275abb281",
     null,
     null,
-    "/team/general/"
+    "/team/general/harikesh.webp"
+  ),
+  new Volunteer(
+    "Vaisnav Rajesh Nair",
+    "https://www.linkedin.com/in/harikrishnan-s-275abb281",
+    null,
+    null,
+    "/team/general/vaishnav.webp"
   ),
 ];
 
